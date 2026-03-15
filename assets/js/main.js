4d6741c13720aac21f8811611b337d86661566ef
Pyrad-Time <macspyrad@gmail.com>
2026-03-14 23:42:35 -0300

    feat: add mobile menu toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),  
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
