ef49c24e036932084f91f1647097c4cf51b42001
Pyrad-Time <macspyrad@gmail.com>
2026-04-01 08:53:08 -0300

    feat(scroll): add scroll-up button (layout, style and bahavior)

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,14 @@ window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
+function scrollUp() {
+    const scrollUp = document.getElementById('scrollup')
+    
+    if(this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
